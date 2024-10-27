2ff941b5364d1a98d79f9de14b0e0c3b16a3eb2d
vivekumar08 <vivekumar2003bsr@gmail.com>
2024-10-27 07:52:05 +0530

    feat(theme): dynamic toggling while changing the themem

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,6 @@
-// components/ThemeToggle.tsx
 "use client";
 import { useTheme } from "@/hooks/ThemeContext";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
@@ -8,8 +8,9 @@ export default function ThemeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className="p-2 border rounded-md bg-gray-200 dark:bg-gray-700 dark:text-white">
-      {theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
+      className="p-2 border rounded-md bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white"
+      aria-label="Toggle Theme">
+      {theme === "light" ? <FaMoon /> : <FaSun />}
     </button>
   );
 }
