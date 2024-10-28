a63ec85764830d4b469a8ac48ad340a761ec1957
vivekumar08 <vivekumar2003bsr@gmail.com>
2024-10-28 21:43:58 +0530

    fix(theme): theme selector with reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import apiSlice from "@/features/apiSlice";
 import authSlice from "@/reducer/auth/authSlice";
+import themeSlice from "@/reducer/theme/themeSlice";
 
 const rootReducer = combineReducers({
   [apiSlice.reducerPath]: apiSlice.reducer,
   auth: authSlice,
+  theme: themeSlice,
 });
 
 export default rootReducer;
