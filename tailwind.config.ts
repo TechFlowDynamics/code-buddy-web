b0c2ac90a261165ebb2c574cf930988b3a832379
vivekumar08 <vivekumar2003bsr@gmail.com>
2024-11-03 11:07:19 +0530

    fix(lint): tailwind css config fixed while building

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        // 'to-top-blue-gray': 'linear-gradient(to top, #1a202c 0%, #537895 100%)',
-        "to-top-blue-gray":
-          " linear-gradient(60deg, #29323c 0%, #485563 100%);",
+        "to-top-blue-gray": "linear-gradient(60deg, #29323c 0%, #485563 100%)",
         "radial-blue-gray":
           "radial-gradient(circle at 10% 20%, rgb(90, 92, 106) 0%, rgb(32, 45, 58) 81.3%)",
       },
