157c7b43bdee2c7104f89566c2eb2f1277d178da
vivekumar08 <vivekumar2003bsr@gmail.com>
2024-11-29 22:40:33 +0530

    feat(dashboard): some data

--- src/components/molecule/cards/ProfileCards.tsx
+++ src/components/molecule/cards/ProfileCards.tsx
@@ -1,9 +1,15 @@
 // src/app/profile/components/ProfileCard.tsx
+'use client'
 import { Avatar } from "@mantine/core";
+import { useSelector } from "react-redux";
 
 import React from "react";
 
+import { RootState } from "@/store/store";
+
 const ProfileCard: React.FC = () => {
+  const userDetails = useSelector((state: RootState) => state.auth);
+  console.log("🚀 ~ userDetails:", userDetails)
   return (
     <div className="w-full max-w-2xl rounded-lg bg-lightBackground bg-opacity-10 p-6 text-center shadow-md backdrop-blur-lg dark:bg-darkBgColor dark:bg-opacity-30">
       <Avatar
@@ -13,8 +19,8 @@ const ProfileCard: React.FC = () => {
         size="md"
         className="mx-auto h-40 w-40 rounded-full border-2 shadow-md"
       />
-      <h2 className="mt-4 text-2xl font-semibold">Vivek Kumar</h2>
-      <p className="text-gray-400">@Vivekumar08</p>
+      <h2 className="mt-4 text-2xl font-semibold">{userDetails.fullName}</h2>
+      <p className="text-gray-400">@{userDetails.userName || "john.doe"}</p>
       <p className="mt-2 text-sm text-gray-300">
         🚀 Full Stack Developer | Tech Innovator <br />
         🎓 B.Tech in IT & Mathematical Innovations, Minor in Management — DU
